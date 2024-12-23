fcd61fa6c603e334bdbeb36826d29a3cecd76649
idlpc <idloco@hotmail.com>
2024-12-22 18:47:33 -0600

    Added Trackball support

--- 6x6_RamiTracktyl/rules.mk
+++ 6x6_RamiTracktyl/rules.mk
@@ -4,13 +4,19 @@
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
+WPM_ENABLE = yes
+
+# Trackball
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3389
+MOUSE_SHARED_EP = yes
 
 SERIAL_DRIVER = vendor
 
